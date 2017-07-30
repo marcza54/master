7c53db86c1341132be46780603916a07d65906bd
Friedrich Lindenberg <friedrich@pudo.org>
2017-07-30 11:39:26 +0200

    try and run this low memory

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import gc
 import json
 import yaml
 import requests
@@ -5,12 +6,15 @@ import requests
 from libsanctions import Source, Entity
 from libsanctions.model import session
 
-NAME = 'combined'
+NAME = 'master'
 YAML_URL = 'https://raw.githubusercontent.com/opensanctions/opensanctions.org/master/_data/sources.yml'  # noqa
 SOURCE_URL = 'http://data.opensanctions.org/v1/sources/%s/latest/%s.ijson'
+IGNORE = ['master', 'everypolitician']
 
 
 def load_source(source_name):
+    if source_name in IGNORE:
+        return
     url = SOURCE_URL % (source_name, source_name)
     res = requests.get(url, stream=True)
     if res.status_code != 200:
@@ -19,6 +23,8 @@ def load_source(source_name):
         data = json.loads(line)
         Entity.from_json(data)
     session.commit()
+    session.remove()
+    gc.collect()
 
 
 def combine():
